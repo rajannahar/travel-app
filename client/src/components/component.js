c26c167854ddec44eec621428fef5bccab49cc33
R_N101 <R_N101@msn.com>
2018-06-14 23:05:11 +0100

    sorting in dev

--- client/src/components/component.js
+++ client/src/components/component.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import {Card, Col, Row} from 'react-materialize'
 import Select from './select';
 
 class Client extends Component {
@@ -12,60 +13,142 @@ class Client extends Component {
 
   componentDidMount() {
       
-      fetch('/api/travel')
-      .then(res => res.json()
-      .then(data => {
-          const hotelData = data.Establishments;
-          this.setState({hotels:hotelData});
-      }));
+    fetch('/api/travel')
+    .then(res => res.json()
+    .then(data => {
+        const hotelData = data.Establishments;
+        this.setState({hotels:hotelData});
+    }));
+
   }
+
+    handleChange(event) {
+        // this.setState({value: event.target.value});
+
+        //let test = [...this.props.hotels];
+        //console.log("...... ",test);
+
+        console.log(event.target.value);
+
+        switch(event.target.value) {
+            case("1"):
+                console.log("Distance - low to high");
+                //newObj.sort((a, b) => a.Distance - b.Distance);
+                break;
+            
+            case("2"):
+                console.log("Distance - high to low");
+                //newObj.sort((a, b) => b.Distance - a.Distance);
+                break;
+            
+            case("3"):
+                console.log("Stars - low to high");
+                //newObj.sort((a, b) => a.Stars - b.Stars);
+                break;
+                
+            case("4"):
+                console.log("Stars - high to low");
+                //newObj.sort((a, b) => b.Stars - a.Stars);
+                break;
+            
+            case("5"):
+                console.log("Min cost - low to high");
+                //newObj.sort((a, b) => a.MinCost - b.MinCost);
+                break;
+
+            case("6"):
+                console.log("Min cost - high to low");
+                //newObj.sort((a, b) => b.MinCost - a.MinCost);
+                break;
+
+            case("7"):
+                console.log("User rating - low to high");
+                //newObj.sort((a, b) => a.UserRating - b.UserRating);
+                break;
+
+            case("8"):
+                console.log("User rating - high to low");
+                //newObj.sort((a, b) => b.UserRating - a.UserRating);
+                break;
+            
+            default:
+                console.log("default");
+
+        }
+    }
 
 
   render() {
 
-    // console.log(this.state.hotels);
-    // const { hotels } = this.state.hotels;
-    // console.log("111 ", hotels);
-
     if (this.state.hotels.length) {
-        console.log(this.state.hotels);
+        //console.log(this.state.hotels);
+
+        let obj1 = [...this.state.hotels]
+        obj1.sort((a, b) => a.Stars - b.Stars);
+        console.log("obj1 ", obj1);
+
+        let obj2 = [...this.state.hotels]
+        obj2.sort((a, b) => b.Stars - a.Stars);
+        console.log("obj2 ", obj2);
+
     }
 
     if (this.state.hotels.length) {
         console.log("load");
+        let newObj = [...this.state.hotels]
         return (
             <div className="client">
                 <div className="container">
         
-                    <div className="row">
-                    <p>ppp</p>
-                        <Select hotels={this.state.hotels} />
-                    </div>
-                    <div className="row">
+                    <Row>
+                        <Select hotels={this.state.hotels} handleChange={this.handleChange} />
+                    </Row>
+                    <Row>
         
-                        {this.state.hotels && this.state.hotels.map(hotel =>
+                        {/* {{this.state.hotels.filter(function(hotel) { return hotel.Stars === 1; })
+                        .map(hotel =>} */}
+
+                        {/* {{this.state.hotels && 
+                        this.state.hotels.map(hotel =>} */}
+
+                        {/* {{this.state.hotels &&
+                        this.state.hotels.sort(function(hotel) { return hotel.Stars === 1; })
+                        .map(hotel =>} */}
+
+                        {/* {var obj = [...this.state.data];
+                        obj.sort((a,b) => a.timeM - b.timeM);
+                        obj.map((item, i) => (<div key={i}> {item.matchID}  
+                                            {item.timeM} {item.description}</div>))} */}
+
                         
-                            <div className="col s12 m6" key={hotel.EstablishmentId}>
-                            <div className="card">
-                                <div className="card-image">
-                                    <img src={hotel.ImageUrl} alt="" />
-                                    <span className="card-title">{hotel.Name}</span>
-                                </div>
-                                <div className="card-content">
-                                    <p><b>Type: </b>{hotel.EstablishmentType}</p>
-                                    <p><b>Location: </b>{hotel.Location}</p>
-                                    <p><b>Minimum cost: </b>£{hotel.MinCost}</p>
-                                    <p><b>Stars: </b>{hotel.Stars}</p>
-                                    <p><b>User rating: </b>{hotel.UserRating}</p>
-                                    <p><b>User rating title: </b>{hotel.UserRatingTitle}</p>
-                                    <p><b>User rating count: </b>{hotel.UserRatingCount}</p>
-                                </div>
-                            </div>
-                        </div>
+                        {
+                        
+                        newObj &&
+                        newObj
+                        .map(hotel =>
+                        
+                            <Col m={6} s={12} key={hotel.EstablishmentId}>
+                                <Card>
+                                    <div className="card-image">
+                                        <img src={hotel.ImageUrl} alt="" />
+                                        <span className="card-title">{hotel.Name}</span>
+                                    </div>
+                                    <div className="card-content">
+                                        <p><b>Type: </b>{hotel.EstablishmentType}</p>
+                                        <p><b>Location: </b>{hotel.Location}</p>
+                                        <p><b>Minimum cost: </b>£{hotel.MinCost}</p>
+                                        <p><b>Stars: </b>{hotel.Stars}</p>
+                                        <p><b>User rating: </b>{hotel.UserRating}</p>
+                                        <p><b>User rating title: </b>{hotel.UserRatingTitle}</p>
+                                        <p><b>User rating count: </b>{hotel.UserRatingCount}</p>
+                                        <p><b>Distance: </b>{hotel.Distance}</p>
+                                    </div>
+                                </Card>
+                            </Col>
         
                         )}
                 
-                    </div>
+                    </Row>
                 </div>
             </div>
         );
--- client/src/components/select.js
+++ client/src/components/select.js
@@ -1,45 +1,42 @@
 import React, { Component } from 'react';
-
+import {Row, Input} from 'react-materialize'
+// import $ from 'jquery';
 class Select extends Component {
 
     constructor(props) {
-        super(props);
-        
-        
+        super(props);    
     }
-    
-    handleChange(event) {
-        // this.setState({value: event.target.value});
-        console.log(event.target.value);
+
+    componentDidMount() {
+        console.log("select mount");
     }
 
 
-  render(props) {
-    if (this.props.length) {
-        console.log("props ", this.props);
+
+    render(props) {
+        if (this.props.hotels.length) {
+            console.log("props ", this.props);
+        }
+
+        return (
+
+            <Row>
+                <Input s={12} type='select' label="Sort by:" defaultValue='1' onChange={this.props.handleChange}>
+                    <option value="1">Distance - low to high</option>
+                    <option value="2">Distance - high to low</option>
+                    <option value="3">Stars - low to high</option>
+                    <option value="4">Stars - high to low</option>
+                    <option value="5">Min Cost - low to high</option>
+                    <option value="6">Min Cost - high to low</option>
+                    <option value="7">User Rating - low to high</option>
+                    <option value="8">User Rating - high to low</option>
+                </Input>
+            </Row>
+            
+            
+            
+        );
     }
-
-    return (
-      
-        <div className="input-field col s12 m6">
-            <select className="select.browser-default" onChange={this.handleChange} >
-                {/* {this.props.hotels.map(hotel =>
-                    <option value={hotel.Stars}>{hotel.Stars}</option>
-                )}  */}
-                <option value="">Sort by:</option>
-                <option value="1">Distance - low to high</option>
-                <option value="2">Distance - high to low</option>
-                <option value="3">Stars - low to high</option>
-                <option value="4">Stars - high to low</option>
-                <option value="5">Min Cost - low to high</option>
-                <option value="6">Min Cost - high to low</option>
-                <option value="7">User Rating - low to high</option>
-                <option value="8">User Rating - high to low</option>
-
-            </select>
-        </div>
-    );
-  }
 }
 
 export default Select;
