3ad441520d86b8f0284cecfd2f24f19f3a9ee797
R_N101 <R_N101@msn.com>
2018-07-15 14:12:35 +0100

    port config, procfile

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require("express");
 // const cors = require('cors');
 
 const app = express();
-const port = 5000;
+const port = process.env.PORT || 5000;
 
 const data = require('./data/hotels.json');
 
