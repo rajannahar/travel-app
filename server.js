91f2d51c7c5e1d8c0e660385882a4685235120f9
R_N101 <R_N101@msn.com>
2018-07-21 18:57:12 +0100

    trying cors

--- server.js
+++ server.js
@@ -1,12 +1,12 @@
 const express = require("express");
-// const cors = require('cors');
+const cors = require('cors');
 
 const app = express();
 const port = process.env.PORT || 5000;
 
 const data = require('./data/hotels.json');
 
-// app.use(cors());
+app.use(cors());
 app.get('/api', (reqeust, response) => {
     response.json(data);
 })
